a9f4c295e42f636bab0484f00e4f8c3e738709dd
NishantMusmade <nishantmusmade.5@gmail.com>
2024-08-23 07:51:15 +0530

    Length of last word

--- try.py
+++ try.py
@@ -1,3 +1,26 @@
-strs = ["flower","flower","flower","flower"]
+s = 'Nishant Nitin Musmade     hello    hiii '
+# i = len(s)-1
+# last_word = ''
+# while s[i]!= ' ':
+#     last_word=last_word+s[i]
+#     i=i-1
 
-print(len(strs[0]))
+# print(len(last_word))
+
+import time
+tic = time.time()
+print(tic)
+s = s.strip()
+# toc = time.time()
+# print(toc)
+# print((toc-tic)*1000)
+# tic = time.time()
+i = len(s)-1
+last_word = ''
+while s[i]!=' ' and i>=0:
+    last_word=last_word+s[i]
+    i=i-1
+
+print(len(last_word))
+toc = time.time()
+print((toc-tic)*1000)
